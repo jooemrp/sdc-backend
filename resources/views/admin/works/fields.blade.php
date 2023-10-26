b929dce017c1289280cbf166106073cdd3206e1f
Andi Fasaya Yaqhsya Ma'ruf <53859179+fasaya@users.noreply.github.com>
2023-10-26 10:30:16 +0800

    Update fields.blade.php

--- resources/views/admin/works/fields.blade.php
+++ resources/views/admin/works/fields.blade.php
@@ -1,26 +1,3 @@
-{{-- <form action="/works/save" method="POST" class="grid grid-cols-1 mb-5">
-    @csrf
-    <h2 class="text-2xl font-semibold my-5">Title</h2>
-    <input type="text" name="title" placeholder="Title"><br>
-    <h2 class="text-2xl font-semibold my-5">Category</h2>
-    <select name="category">
-        <option value="">ChooseCategory</option>
-        <option value="Content Creation">Content Creation</option>
-        <option value="Paid Marketing">Paid Marketing</option>
-        <option value="Content Marketing">Content Marketing</option>
-    </select><br>
-    <h2 class="text-2xl font-semibold my-5">The Challenge</h2>
-    <textarea name="challenge" rows="10" placeholder="Challenge"></textarea><br>
-    <h2 class="text-2xl font-semibold my-5">Our Approach</h2>
-    <textarea name="approach" rows="10" placeholder="Approach"></textarea><br>
-    <h2 class="text-2xl font-semibold my-5">The Result</h2>
-    <textarea name="result" rows="10" placeholder="Result"></textarea><br>
-    <button class="mx-auto bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 w-1/5 rounded-xl text-xl pb">
-        <input type="submit" name="submit" value="Save">
-    </button>
-</form> --}}
-
-
 @csrf
 <div class="form-group row">
 
@@ -33,9 +10,9 @@
         <label>Category *</label>
         <select name="category" id="category" class="form-control">
             <option value="">-- Select category --</option>
-            <option value="Content Creation">Content Creation</option>
-            <option value="Paid Marketing">Paid Marketing</option>
-            <option value="Content Marketing">Content Marketing</option>
+            <option value="Content Creation" {{ isset($data) && $data->category == 'Content Creation' }}>Content Creation</option>
+            <option value="Paid Marketing" {{ isset($data) && $data->category == 'Paid Marketing' }}>Paid Marketing</option>
+            <option value="Content Marketing" {{ isset($data) && $data->category == 'Content Marketing' }}>Content Marketing</option>
         </select>
     </div>
 
