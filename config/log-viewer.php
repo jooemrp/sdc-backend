d5c28c36a4165edae2da433d03cbd82653943cf1
Andi Fasaya Yaqhsya Ma'ruf <53859179+fasaya@users.noreply.github.com>
2023-11-06 09:12:22 +0800

    Create log-viewer.php

---
+++ config/log-viewer.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |------------------------------------------------------------------
+    | Log Viewer Route
+    |------------------------------------------------------------------
+    | Log Viewer will be available under this URL.
+    |
+    */
+    'route_path' => 'admin/log-viewer',
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Log Viewer route middleware.
+    |--------------------------------------------------------------------------
+    | The middleware should enable session and cookies support in order for the Log Viewer to work.
+    | The 'web' middleware will be applied automatically if empty.
+    |
+    */
+    'middleware' => ['role:admin', 'auth']
+];
